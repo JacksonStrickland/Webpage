22a1f23e7229a9f464e0918407c1b0b11cd063ca
JacksonStrickland <godelila@gmail.com>
2024-10-03 17:46:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-// Function to switch between tabs
-function openTab(event, tabName) {
-    // Hide all tab content
-    var i, tabContent, tabLinks;
-    tabContent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none";
-    }
 
-    // Remove active class from all tab links
-    tabLinks = document.querySelectorAll('nav ul li a');
-    for (i = 0; i < tabLinks.length; i++) {
-        tabLinks[i].classList.remove("active");
-    }
-
-    // Show the selected tab and add active class to clicked tab link
-    document.getElementById(tabName).style.display = "block";
-    event.currentTarget.classList.add("active");
-}
